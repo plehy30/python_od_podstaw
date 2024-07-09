5006608f9fff1d12a52f936db47393eea32296c5
plehy30 <plehy30@gmail.com>
2024-07-09 16:41:13 +0200

    7_10

--- 19_funkcje.py
+++ 19_funkcje.py
@@ -1,12 +1,6 @@
-def hello(name, age, last_name=None):
-    print('Cześć ' + name + ' ! Masz ' + str(age) + ' lat.')
-    # print('Masz na nazwisko: ' + last_name)
-    if last_name is not None:
-        print('Masz na nazwisko: ' + last_name)
+#         print('Masz na nazwisko: ' + last_name)
 
 # hello(input('Podaj imię: '), 35)
 # hello(input('Podaj imię: '), 40)
 # hello(name=input('Podaj imię:'), age=35, last_name='Leczkowski')
 # hello(name=input('Podaj imię: '), age=40, last_name='Brzeziński')
-hello(name=input('Podaj imię:'), age=35, last_name='Leczkowski')
-hello(name=input('Podaj imię: '), age=40,)
